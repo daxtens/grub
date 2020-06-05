067472ad957df6c0db2dcaf3c87396e03bf5c143
Daniel Axtens <dja@axtens.net>
2020-06-05 17:47:25 +1000

    wrap file with license
    Signed-off-by: Daniel Axtens <dja@axtens.net>

---
+++ grub-core/lib/libtasn1_wrap/wrap.c
@@ -0,0 +1,8 @@
+#include <grub/dl.h>
+
+/*
+ * libtasn1 is provided under LGPL2.1+, which is compatible
+ * with GPL3+. As Grub as a whole is under GPL3+, this module
+ * is therefore under GPL3+ also.
+ */
+GRUB_MOD_LICENSE ("GPLv3+")
